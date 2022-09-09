12bbe8ac3704cc85dcfe3862633e5567b24195fe
nitya-dyuthi <110531551+nitya-dyuthi@users.noreply.github.com>
2022-09-09 12:22:56 +0530

    clean up checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -3,44 +3,27 @@
 using namespace std;
 
 bool isTempOk(float temperature) {
-  if(temperature > 0 || temperature < 45) {
-    //cout << "Temperature out of range!\n";
+  if(temperature > 0 && temperature < 45) {
     return true;
   }
-  else
     return false;
 }
 
 bool isSocOk(float soc) {
-  if(soc > 20 || soc < 80) {
-    //cout << "State of Charge out of range!\n";
+  if(soc > 20 && soc < 80) {
     return true;
   }
-  else
     return false;
 }
 
 bool isChargeRateOk(float chargeRate) {
   if(chargeRate < 0.8) {
-    //cout << "Charge Rate out of range!\n";
     return true;
   }
-  else
     return false;
 }
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) { //aka delegator
-  /* if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  return true; */
   
   bool tempOk = isTempOk(temperature);
   bool socOk = isSocOk(soc);
